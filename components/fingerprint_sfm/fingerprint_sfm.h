2e77d959eced362c8deea460473e9ca1e1570385
vasyna <vasyna@gmail.com>
2026-08-24 11:27:19 +0300

    Update fingerprint_sfm.h
    #include "esphome/core/trigger.h"

--- components/fingerprint_sfm/fingerprint_sfm.h
+++ components/fingerprint_sfm/fingerprint_sfm.h
@@ -4,6 +4,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
+#include "esphome/core/trigger.h"
 #include <memory>
 #include <queue>
 
